ae96ea4907bc3e6d39299acc839b69fef687b8b6
Michael Chadwick <michael.chadwick@gmail.com>
2024-05-14 15:59:21 -0700

    added Counter and DoubleIt components to test arguments and actions

---
+++ app/components/counter.js
@@ -0,0 +1,21 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class CounterComponent extends Component {
+  @tracked count = 0;
+
+  get total() {
+    return this.count * this.args.multiple;
+  }
+
+  @action
+  change(amount) {
+    this.count += amount;
+  }
+
+  @action
+  double() {
+    this.args.updateMultiple(this.args.multiple * 2);
+  }
+}
