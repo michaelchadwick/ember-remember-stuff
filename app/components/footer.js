4176ac34e61817f81b618b3b3365c2ca980d77ac
Michael Chadwick <michael.chadwick@gmail.com>
2024-05-16 15:33:36 -0700

    added footer link to author

--- app/components/footer.js
+++ app/components/footer.js
@@ -5,6 +5,13 @@ export default class FooterComponent extends Component {
     const links = [];
 
     links.push({
+      url: 'https://michaelchadwick.info',
+      route: 'author',
+      title: 'Author',
+      target: '_blank',
+    });
+
+    links.push({
       url: 'https://github.com/michaelchadwick/ember-fake-chat',
       route: 'source',
       title: 'Github Source',
