8162b108fd2288cddd7cc15a32653557b6eea745
Michael Chadwick <michael.chadwick@gmail.com>
2024-05-20 11:39:53 -0700

    added Contact controller

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,18 +1,3 @@
 import Route from '@ember/routing/route';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 
-export default class ContactRoute extends Route {
-  @tracked name;
-  @tracked email;
-  @tracked message;
-
-  @action
-  sendMessage(event) {
-    event.preventDefault();
-
-    if (this.name && this.email && this.message) {
-      console.log('contact submission', this.name, this.email, this.message);
-    }
-  }
-}
+export default class ContactRoute extends Route {}
