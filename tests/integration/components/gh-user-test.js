b0db94274609c873a05c3034f3ffc0e7c600410e
Michael Chadwick <michael.chadwick@gmail.com>
2024-05-21 16:16:19 -0700

    fixed tests

--- tests/integration/components/gh-user-test.js
+++ tests/integration/components/gh-user-test.js
@@ -6,21 +6,9 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | gh-user', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it renders with a username', async function (assert) {
+    await render(hbs`<GhUser @username='michaelchadwick' />`);
 
-    await render(hbs`<GhUser />`);
-
-    assert.dom().hasText('');
-
-    // Template block usage:
-    await render(hbs`
-      <GhUser>
-        template block text
-      </GhUser>
-    `);
-
-    assert.dom().hasText('template block text');
+    assert.dom().hasText('GithubUser michaelchadwick (repos: )');
   });
 });
