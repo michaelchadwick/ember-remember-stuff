52538aabb8090cf4125d1057381a6ba5a4e4283a
Michael Chadwick <michael.chadwick@gmail.com>
2024-05-21 08:41:37 -0700

    added Ribbon component

---
+++ tests/integration/components/ribbon-test.js
@@ -0,0 +1,19 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-fake-chat/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | ribbon', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    await render(hbs`<Ribbon />`);
+
+    assert.dom('.ribbon').exists();
+
+    await render(hbs`<Ribbon @position="left" @content="Foobar" />`);
+
+    assert.dom('.ribbon').hasClass('left');
+    assert.dom('.ribbon').hasText('Foobar');
+  });
+});
