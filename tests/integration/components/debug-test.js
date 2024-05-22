ebd4b16f4d3e5d8ca26a46a8a07efaeed998e16e
Michael Chadwick <michael.chadwick@gmail.com>
2024-05-22 15:53:58 -0700

    added Debug component to put all my random component tests in

---
+++ tests/integration/components/debug-test.js
@@ -0,0 +1,14 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'remember-stuff/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | debug', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    await render(hbs`<Debug />`);
+
+    assert.dom('details').hasClass('debug');
+  });
+});
