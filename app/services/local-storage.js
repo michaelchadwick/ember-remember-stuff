4bd121bc30cba7acb40da3401271c58a07fc20a1
Michael Chadwick <michael.chadwick@gmail.com>
2024-05-24 16:32:52 -0700

    added new LocalStorage service

---
+++ app/services/local-storage.js
@@ -0,0 +1,15 @@
+import Service from '@ember/service';
+import ENV from 'remember-stuff/config/environment';
+
+export default class LocalStorageService extends Service {
+  settings = JSON.parse(localStorage.getItem(ENV.APP.LOCAL_STORAGE_KEY)) || {};
+
+  get(key) {
+    return this.settings[key];
+  }
+
+  set(key, val) {
+    this.settings[key] = val;
+    localStorage.setItem(ENV.APP.LOCAL_STORAGE_KEY, JSON.stringify(this.settings));
+  }
+}
