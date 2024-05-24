3402e08c2f28fc8d840b5a71cafae11c24aa27d5
Michael Chadwick <michael.chadwick@gmail.com>
2024-05-24 15:21:04 -0700

    made sure all checklists have summaries

--- app/models/checklist.js
+++ app/models/checklist.js
@@ -2,6 +2,7 @@ import Model, { attr } from '@ember-data/model';
 
 export default class ChecklistModel extends Model {
   @attr('string') title;
+  @attr('string') summary;
   @attr('boolean') checked;
   @attr lists;
 }
