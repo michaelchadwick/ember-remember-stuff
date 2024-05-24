ae67f144de6a8107c9fdd2f37718a132b51275c5
Michael Chadwick <michael.chadwick@gmail.com>
2024-05-24 15:07:56 -0700

    fixed tests

--- tests/integration/components/message-test.js
+++ tests/integration/components/message-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { setupIntl } from 'ember-intl/test-support';
+import { setupIntl, t } from 'ember-intl/test-support';
 import { setupRenderingTest } from 'remember-stuff/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
@@ -11,19 +11,20 @@ module('Integration | Component | message', function (hooks) {
   test('it renders', async function (assert) {
     await render(hbs`
       <Message
-        @id="message-0"
+        @id='message-0'
         @username='Tomster'
         @userIsActive={{true}}
         @userIsCurrent={{false}}
-        @userLocalTime="5:15 PM"
+        @userLocalTime='5:15 PM'
       >
-        <p>{{t "general.hello" name="Tomster"}}</p>
+        <p>{{t 'general.hello' name='Tomster'}}</p>
       </Message>
     `);
 
     assert.dom('.message').exists();
     assert.dom('.message aside').exists();
+    assert.dom('.message aside .avatar.is-active').exists();
     assert.dom('.message section').exists();
-    assert.dom('.message section p').hasText("Hello, I'm Tomster");
+    assert.dom('.message section p').hasText(t('general.hello', { name: 'Tomster' }));
   });
 });
