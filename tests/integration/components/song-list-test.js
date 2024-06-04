a41710e6ba5f5a4bf6a2b48793218005db324fa2
Michael Chadwick <michael.chadwick@gmail.com>
2024-06-04 16:44:28 -0700

    fixed test

--- tests/integration/components/song-list-test.js
+++ tests/integration/components/song-list-test.js
@@ -1,13 +1,19 @@
 import { module, test } from 'qunit';
+import { setupIntl } from 'ember-intl/test-support';
 import { setupRenderingTest } from 'remember-stuff/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | song-list', function (hooks) {
   setupRenderingTest(hooks);
+  setupIntl(hooks, 'en-us');
 
   test('it renders', async function (assert) {
-    await render(hbs`<SongList />`);
+    const songs = await this.owner.lookup('service:store').findAll('song');
+
+    this.set('songs', songs);
+
+    await render(hbs`<SongList @songs={{this.songs}}/>`);
 
     assert.dom('.song-list').exists();
   });
