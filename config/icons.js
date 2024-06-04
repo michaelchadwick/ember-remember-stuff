b0d149ca5620c5007855802970b2cc1b5140ab78
Michael Chadwick <michael.chadwick@gmail.com>
2024-06-04 13:48:04 -0700

    added circle-info and ear-listen fontawesome icons

--- config/icons.js
+++ config/icons.js
@@ -1,6 +1,15 @@
 module.exports = function () {
   return {
-    'free-solid-svg-icons': ['at', 'book', 'flask', 'pause', 'play', 'square-up-right'],
+    'free-solid-svg-icons': [
+      'at',
+      'book',
+      'circle-info',
+      'ear-listen',
+      'flask',
+      'pause',
+      'play',
+      'square-up-right',
+    ],
     'free-brands-svg-icons': ['github'],
   };
 };
