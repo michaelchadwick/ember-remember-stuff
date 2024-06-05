f5dfbc61dfe5319685d986b13f54af1732eb7240
Michael Chadwick <michael.chadwick@gmail.com>
2024-06-05 16:30:08 -0700

    About route now shows most recent 5 commits of github source

--- app/components/gh-commits.js
+++ app/components/gh-commits.js
@@ -3,8 +3,4 @@ import { service } from '@ember/service';
 
 export default class GhCommitsComponent extends Component {
   @service store;
-
-  model() {
-    return this.store.findAll('link');
-  }
 }
--- app/routes/about.js
+++ app/routes/about.js
@@ -1,14 +1,26 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import ENV from 'remember-stuff/config/environment';
 
 export default class AboutRoute extends Route {
   @service store;
   @service headData;
   @tracked isLoading = true;
 
-  model() {
-    // return this.store.findAll('gh-commit');
+  async model() {
+    const repo = 'ember-remember-stuff';
+    const url = `${ENV.APP.GITHUB_API_URL}/repos/${ENV.APP.GITHUB_USERNAME}/${repo}/commits?sort=updated&per_page=5`;
+
+    const response = await fetch(url);
+    const commits = await response.json();
+
+    console.log(
+      'commits',
+      commits.map((c) => c.commit),
+    );
+
+    return commits;
   }
 
   afterModel() {
