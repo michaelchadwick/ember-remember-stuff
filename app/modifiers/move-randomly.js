29575125ac1ae24329a01623d138ac050947dddc
Michael Chadwick <michael.chadwick@gmail.com>
2024-07-01 13:27:23 -0700

    added mover component to test move-randomly modifier

--- app/modifiers/move-randomly.js
+++ app/modifiers/move-randomly.js
@@ -1,13 +1,19 @@
 import { modifier } from 'ember-modifier';
 
-const { random, round } = Math;
+const { ceil, floor, random, round } = Math;
+
+const randInt = (min, max) => {
+  const minCeiled = ceil(min);
+  const maxFloored = floor(max);
+  return round(random() * (maxFloored - minCeiled + 1) + minCeiled);
+};
 
 export default modifier((element) => {
   const id = setInterval(() => {
-    const top = round(random() * 500);
-    const left = round(random() * 500);
+    const top = randInt(-3, 3);
+    const left = randInt(-3, 3);
     element.style.transform = `translate(${left}px, ${top}px)`;
-  }, 1000);
+  }, 100);
 
   return () => clearInterval(id);
 });
