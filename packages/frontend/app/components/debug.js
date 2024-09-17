3f1f67808120198eb39eeb1e14ca246377d790bb
Michael Chadwick <michael.chadwick@gmail.com>
2024-09-17 14:00:21 -0700

    Debug component now scrolls window to top of it when clicked open

--- packages/frontend/app/components/debug.js
+++ packages/frontend/app/components/debug.js
@@ -2,19 +2,23 @@ import Component from '@glimmer/component';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import scrollIntoView from 'scroll-into-view';
 import ENV from 'frontend/config/environment';
 
 export default class DebugComponent extends Component {
+  @service('local-storage') ls;
   @tracked env = ENV.environment;
   @tracked envApp = ENV.APP;
   @tracked isExpanded = this.args.isExpanded;
 
-  @service('local-storage') ls;
-
   @action
-  saveDebugVisibility() {
+  saveDebugVisibility(element) {
     this.isExpanded = !this.isExpanded;
 
     this.ls.set('debugExpanded', this.isExpanded);
+
+    scrollIntoView(element.target, {
+      align: { top: 0 },
+    });
   }
 }
