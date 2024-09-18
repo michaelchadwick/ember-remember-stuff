4ca524a0972f86f03a98038958db16c8b4aa266c
Michael Chadwick <michael.chadwick@gmail.com>
2024-09-18 14:56:29 -0700

    put messages tracked var up with other tracked vars

--- packages/frontend/app/components/messages.js
+++ packages/frontend/app/components/messages.js
@@ -5,6 +5,43 @@ import { DateTime } from 'luxon';
 
 export default class MessagesComponent extends Component {
   @tracked username = 'anonymous';
+  @tracked messages = [
+    {
+      username: 'Tomster',
+      active: true,
+      localTime: this.tomsterLocalTime,
+      content: `
+        <p>
+          Hey Zoey, did you look at the <strong>DecEmberConfRants</strong> brainstorming doc I sent from California, USA?
+        </p>
+      `,
+    },
+    {
+      username: 'Zoey',
+      active: false,
+      localTime: this.zoeyLocalTime,
+      current: true,
+      content: `
+        <p>Hallo from Berlin!
+          I love the ideas! I'm really excited about where this year's
+          <strong>DecEmberConfRants</strong> is going, I'm sure it's going to be the best one yet. Some quick notes:
+        </p>
+
+        <ul>
+          <li>
+            A personal plane for each attendee would definitely make the getting to the venue ultra cool, but I think it might be a bit out of our budget. Maybe we could rent some electric scooters instead?
+          </li>
+          <li>
+            We absolutely will need more cheese wheels as last year's complaints
+            <em>really</em> rattled our sponsors. Will get on that now before dairy
+            season hits its peak.
+          </li>
+        </ul>
+
+        <p>Let me know when you've nailed down the dates!</p>
+      `,
+    },
+  ];
 
   // Tomster: Los Angeles, CA, USA
   get tomsterLocalTime() {
@@ -55,42 +92,4 @@ export default class MessagesComponent extends Component {
       },
     ];
   }
-
-  @tracked messages = [
-    {
-      username: 'Tomster',
-      active: true,
-      localTime: this.tomsterLocalTime,
-      content: `
-        <p>
-          Hey Zoey, did you look at the <strong>DecEmberConfRants</strong> brainstorming doc I sent from California, USA?
-        </p>
-      `,
-    },
-    {
-      username: 'Zoey',
-      active: false,
-      localTime: this.zoeyLocalTime,
-      current: true,
-      content: `
-        <p>Hallo from Berlin!
-          I love the ideas! I'm really excited about where this year's
-          <strong>DecEmberConfRants</strong> is going, I'm sure it's going to be the best one yet. Some quick notes:
-        </p>
-
-        <ul>
-          <li>
-            A personal plane for each attendee would definitely make the getting to the venue ultra cool, but I think it might be a bit out of our budget. Maybe we could rent some electric scooters instead?
-          </li>
-          <li>
-            We absolutely will need more cheese wheels as last year's complaints
-            <em>really</em> rattled our sponsors. Will get on that now before dairy
-            season hits its peak.
-          </li>
-        </ul>
-
-        <p>Let me know when you've nailed down the dates!</p>
-      `,
-    },
-  ];
 }
