82f8585842c028d1a5bb4ae0f6da6bd449918782
Michael Chadwick <michael.chadwick@gmail.com>
2024-09-19 07:49:27 -0700

    updated BoxGallery's Box-es default inventory

--- packages/frontend/app/components/box-gallery.js
+++ packages/frontend/app/components/box-gallery.js
@@ -11,13 +11,10 @@ export default class BoxGallery extends Component {
     {
       block: null,
       text: `
-        <p>Hello. This is a Box component. This text is coming from the Box component <code>@text</code> argument, and supports HTML. It also has a lot of text in it and is probably too tall, so it should be truncated/faded somehow. Here is some more text, and some more text, and some more text, and some more text, and some more text, just in case it needs it to be super duper tall. If that was not enough text, then I will throw in a list.</p>
+        <p>Hello. This is a Box component. This text is coming from the Box component <code>@text</code> argument, and supports HTML.</p>
+        <p>It also has a lot of text in it and is probably too tall, so it should be truncated/faded somehow. Here is some more text, and some more text, and some more text, and some more text, and some more text, just in case it needs it to be super duper tall. If that was not enough text, then I will throw in a list.</p>
         <ul>
           <li>One</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
           <li>Two</li>
           <li>Two</li>
           <li>Two</li>
@@ -31,50 +28,6 @@ export default class BoxGallery extends Component {
       block: `
         <p>Hey! This is a Box component. This text is coming from a block passed into the Box componenet. It <b>does not</b> support <code>HTML</code>.</p>
         <p>However, I'm going to put HTML in here anyway. Nyeh!</p>
-        <ul>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-          <li>Two</li>
-        </ul>
       `,
       text: null,
     },
@@ -90,7 +43,6 @@ export default class BoxGallery extends Component {
     };
 
     if (newBox) {
-      console.log('newBox created', newBox);
       this.boxes = [newBox, ...this.boxes];
     }
 
