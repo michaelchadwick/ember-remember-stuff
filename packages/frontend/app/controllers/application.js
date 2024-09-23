de944cd307585dea630c64e38f278e40f7a3da6d
Michael Chadwick <michael.chadwick@gmail.com>
2024-09-23 11:51:03 -0700

    moved debug and boxgallery components to Index route only; fixed initialization of LS

--- packages/frontend/app/controllers/application.js
+++ packages/frontend/app/controllers/application.js
@@ -1,12 +1,3 @@
 import Controller from '@ember/controller';
-import { service } from '@ember/service';
 
-export default class ApplicationController extends Controller {
-  @service localStorage;
-
-  get debugVisibility() {
-    const debugExpanded = this.localStorage.get('debugExpanded');
-
-    return debugExpanded;
-  }
-}
+export default class ApplicationController extends Controller {}
--- packages/rs-common/addon/services/local-storage.js
+++ packages/rs-common/addon/services/local-storage.js
@@ -2,14 +2,28 @@ import Service from '@ember/service';
 import ENV from 'frontend/config/environment';
 
 export default class LocalStorageService extends Service {
-  settings = JSON.parse(localStorage.getItem(ENV.APP.LOCAL_STORAGE_KEY)) || {};
+  constructor() {
+    super(...arguments);
+
+    if (!localStorage.getItem(ENV.APP.LOCAL_STORAGE_KEY)) {
+      console.log('initializing LS');
+      localStorage.setItem(
+        ENV.APP.LOCAL_STORAGE_KEY,
+        JSON.stringify({
+          debugExpanded: false,
+        }),
+      );
+    }
+  }
 
   get(key) {
-    return this.settings[key];
+    const settings = JSON.parse(localStorage.getItem(ENV.APP.LOCAL_STORAGE_KEY));
+    return settings[key];
   }
 
   set(key, val) {
-    this.settings[key] = val;
-    localStorage.setItem(ENV.APP.LOCAL_STORAGE_KEY, JSON.stringify(this.settings));
+    const settings = JSON.parse(localStorage.getItem(ENV.APP.LOCAL_STORAGE_KEY));
+    settings[key] = val;
+    localStorage.setItem(ENV.APP.LOCAL_STORAGE_KEY, JSON.stringify(settings));
   }
 }
