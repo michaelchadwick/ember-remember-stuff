810554c8a44b992f7de412810f843f6fdc5eaa95
Michael Chadwick <michael.chadwick@gmail.com>
2024-09-24 11:32:39 -0700

    fixed bug where Box unfaded still had a fade

--- packages/frontend/tests/integration/components/box-test.js
+++ packages/frontend/tests/integration/components/box-test.js
@@ -24,25 +24,25 @@ module('Integration | Component | box', function (hooks) {
 
   test('it fades tall text', async function (assert) {
     const longText = `
-        <p>Hello. This is a Box component. This text is coming from the Box component <code>@text</code> argument, and supports HTML.</p>
-          <p>It also has a lot of text in it and is probably too tall, so it should be truncated/faded somehow. Here is some more text, and some more text, and some more text, and some more text, and some more text, just in case it needs it to be super duper tall. If that was not enough text, then I will throw in a list.</p>
-          <p>Another paragraph to take up height? Don't mind if I put that into the template that gets rendered on the page.</p>
-          <p>Another paragraph to take up height? Don't mind if I put that into the template that gets rendered on the page.</p>
-          <p>Another paragraph to take up height? Don't mind if I put that into the template that gets rendered on the page.</p>
-          <ul>
-            <li>One</li>
-            <li>Dos</li>
-            <li>3</li>
-            <li>Quatre</li>
-            <li>Funf</li>
-            <li>667</li>
-            <li>Heaven</li>
-            <li>Ate</li>
-            <li>Nove</li>
-            <li>Binary 3</li>
-          </ul>
-          <p>Cool list, eh?</p>
-      `;
+      <p>Hello. This is a Box component. This text is coming from the Box component <code>@text</code> argument, and supports HTML.</p>
+      <p>It also has a lot of text in it and is probably too tall, so it should be truncated/faded somehow. Here is some more text, and some more text, and some more text, and some more text, and some more text, just in case it needs it to be super duper tall. If that was not enough text, then I will throw in a list.</p>
+      <p>Another paragraph to take up height? Don't mind if I put that into the template that gets rendered on the page.</p>
+      <p>Another paragraph to take up height? Don't mind if I put that into the template that gets rendered on the page.</p>
+      <p>Another paragraph to take up height? Don't mind if I put that into the template that gets rendered on the page.</p>
+      <ul>
+        <li>One</li>
+        <li>Dos</li>
+        <li>3</li>
+        <li>Quatre</li>
+        <li>Funf</li>
+        <li>667</li>
+        <li>Heaven</li>
+        <li>Ate</li>
+        <li>Nove</li>
+        <li>Binary 3</li>
+      </ul>
+      <p>Cool list, eh?</p>
+    `;
     const fadedClass = 'is-faded';
     this.set('text', longText);
 
@@ -57,32 +57,30 @@ module('Integration | Component | box', function (hooks) {
 
   test('expand/collapse', async function (assert) {
     const longHtml = `
-        <p>Hello. This is a Box component. This text is coming from the Box component <code>@text</code> argument, and supports HTML.</p>
-          <p>It also has a lot of text in it and is probably too tall, so it should be truncated/faded somehow. Here is some more text, and some more text, and some more text, and some more text, and some more text, just in case it needs it to be super duper tall. If that was not enough text, then I will throw in a list.</p>
-          <p>Another paragraph to take up height? Don't mind if I put that into the template that gets rendered on the page.</p>
-          <p>Another paragraph to take up height? Don't mind if I put that into the template that gets rendered on the page.</p>
-          <p>Another paragraph to take up height? Don't mind if I put that into the template that gets rendered on the page.</p>
-          <ul>
-            <li>One</li>
-            <li>Dos</li>
-            <li>3</li>
-            <li>Quatre</li>
-            <li>Funf</li>
-            <li>667</li>
-            <li>Heaven</li>
-            <li>Ate</li>
-            <li>Nove</li>
-            <li>Binary 3</li>
-          </ul>
-          <p>Cool list, eh?</p>
-      `;
+      <p>Hello. This is a Box component. This text is coming from the Box component <code>@text</code> argument, and supports HTML.</p>
+      <p>It also has a lot of text in it and is probably too tall, so it should be truncated/faded somehow. Here is some more text, and some more text, and some more text, and some more text, and some more text, just in case it needs it to be super duper tall. If that was not enough text, then I will throw in a list.</p>
+      <p>Another paragraph to take up height? Don't mind if I put that into the template that gets rendered on the page.</p>
+      <p>Another paragraph to take up height? Don't mind if I put that into the template that gets rendered on the page.</p>
+      <p>Another paragraph to take up height? Don't mind if I put that into the template that gets rendered on the page.</p>
+      <ul>
+        <li>One</li>
+        <li>Dos</li>
+        <li>3</li>
+        <li>Quatre</li>
+        <li>Funf</li>
+        <li>667</li>
+        <li>Heaven</li>
+        <li>Ate</li>
+        <li>Nove</li>
+        <li>Binary 3</li>
+      </ul>
+      <p>Cool list, eh?</p>
+    `;
     const longText =
       "Hello. This is a Box component. This text is coming from the Box component @text argument, and supports HTML. It also has a lot of text in it and is probably too tall, so it should be truncated/faded somehow. Here is some more text, and some more text, and some more text, and some more text, and some more text, just in case it needs it to be super duper tall. If that was not enough text, then I will throw in a list. Another paragraph to take up height? Don't mind if I put that into the template that gets rendered on the page. Another paragraph to take up height? Don't mind if I put that into the template that gets rendered on the page. Another paragraph to take up height? Don't mind if I put that into the template that gets rendered on the page. One Dos 3 Quatre Funf 667 Heaven Ate Nove Binary 3 Cool list, eh?";
     const fadedClass = 'is-faded';
     this.set('text', longHtml);
     await render(hbs`<Box @text={{this.text}} />`);
-
-    // await pauseTest();
 
     assert.dom('.display-text-wrapper', this.element).hasClass(fadedClass);
     assert.strictEqual(component.text, longText);
@@ -91,15 +89,11 @@ module('Integration | Component | box', function (hooks) {
 
     await component.expand.click();
 
-    // await pauseTest();
-
     assert.dom('.display-text-wrapper', this.element).doesNotHaveClass(fadedClass);
     assert.notOk(component.expand.isVisible);
     assert.ok(component.collapse.isVisible);
 
     await component.collapse.click();
-
-    // await pauseTest();
 
     assert.dom('.display-text-wrapper', this.element).hasClass(fadedClass);
     assert.ok(component.expand.isVisible);
