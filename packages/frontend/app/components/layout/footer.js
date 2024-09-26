07893bc1781ab1825748d81f2fd3284707b93c77
Michael Chadwick <michael.chadwick@gmail.com>
2024-09-26 13:22:08 -0700

    refactored footer links

--- packages/frontend/app/components/layout/footer.js
+++ packages/frontend/app/components/layout/footer.js
@@ -5,53 +5,54 @@ import { service } from '@ember/service';
 export default class FooterComponent extends Component {
   @service intl;
 
+  defaultLink = {
+    iconType: 'fas',
+    target: '_blank',
+  };
+
   get links() {
     const links = [];
 
     links.push({
+      ...this.defaultLink,
       url: 'https://michaelchadwick.info',
       route: 'author',
       icon: 'at',
-      iconType: 'fas',
       title: this.intl.t('layout.footAuthor'),
-      target: '_blank',
     });
 
     links.push({
+      ...this.defaultLink,
       url: 'https://github.com/michaelchadwick/ember-remember-stuff',
       route: 'source',
       icon: 'github',
       iconType: 'fab',
       title: this.intl.t('layout.footSource'),
-      target: '_blank',
     });
 
     links.push({
+      ...this.defaultLink,
       url: 'https://guides.emberjs.com/release/components/',
       route: 'tutorial',
       icon: 'book',
-      iconType: 'fas',
       title: this.intl.t('layout.footDocs'),
-      target: '_blank',
     });
 
     if (ENV.environment != 'production') {
       links.push({
+        ...this.defaultLink,
         url: '/tests?nocontainer',
         route: 'tests',
         icon: 'flask',
-        iconType: 'fas',
         title: `[${this.intl.t('layout.footTests')}]`,
-        target: '_blank',
       });
 
       links.push({
+        ...this.defaultLink,
         url: 'https://mc-emberjs-remember-stuff.netlify.app',
         route: 'prod',
         icon: 'square-up-right',
-        iconType: 'fas',
         title: `[${this.intl.t('layout.footProd')}]`,
-        target: '_blank',
       });
     }
 
