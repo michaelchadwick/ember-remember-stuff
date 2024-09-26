d754bc7c86d97c1b56eb06bf5a33ea31204a67c9
Michael Chadwick <michael.chadwick@gmail.com>
2024-09-26 09:44:51 -0700

    removed duplicate model code

--- packages/frontend/app/models/checklist.js
+++
@@ -1,8 +0,0 @@
-import Model, { attr } from '@ember-data/model';
-
-export default class ChecklistModel extends Model {
-  @attr('string') title;
-  @attr('string') summary;
-  @attr('boolean') checked;
-  @attr lists;
-}
--- packages/frontend/app/models/link.js
+++
@@ -1,7 +0,0 @@
-import Model, { attr } from '@ember-data/model';
-
-export default class LinkModel extends Model {
-  @attr url;
-  @attr title;
-  @attr target;
-}
