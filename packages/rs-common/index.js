fcf42a92740f6dc4f58f49a476d2316624623b82
Michael Chadwick <michael.chadwick@gmail.com>
2024-10-02 14:18:06 -0700

    added some user mirage-support factories/models and configured app to use them

--- packages/rs-common/index.js
+++ packages/rs-common/index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const Funnel = require('broccoli-funnel');
+const MergeTrees = require('broccoli-merge-trees');
+const path = require('path');
+
 module.exports = {
   name: require('./package').name,
 
@@ -10,6 +14,16 @@ module.exports = {
         require.resolve('ember-concurrency/async-arrow-task-transform'),
       ],
     },
+  },
+
+  treeForApp(appTree) {
+    const trees = [appTree];
+    if (['test', 'development'].includes(this._env)) {
+      const mirageDir = path.join(__dirname, 'addon-mirage-support');
+      const mirageTree = new Funnel(mirageDir, { destDir: 'tests/test-support/mirage' });
+      trees.push(mirageTree);
+    }
+    return MergeTrees(trees);
   },
 
   treeForAddonTestSupport(tree) {
