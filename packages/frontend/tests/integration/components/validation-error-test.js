72f1c9d9d6950ada8e2e8e6abeb56b5deda304ad
Michael Chadwick <michael.chadwick@gmail.com>
2024-10-09 09:32:36 -0700

    fixed import path

--- packages/frontend/tests/integration/components/validation-error-test.js
+++ packages/frontend/tests/integration/components/validation-error-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { setupRenderingTest } from 'test-app/tests/helpers';
+import { setupRenderingTest } from 'frontend/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
