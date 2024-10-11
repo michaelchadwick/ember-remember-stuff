35e3c3535f6dce96f6c2105b557a25e388ad5039
Michael Chadwick <michael.chadwick@gmail.com>
2024-10-11 09:28:02 -0700

    add strength checker and simple test to PasswordValidator component; turn component on

---
+++ packages/frontend/tests/integration/components/password-validator-test.js
@@ -0,0 +1,44 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'frontend/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+import { component } from 'frontend/tests/pages/components/password-validator';
+
+module('Integration | Component | password-validator', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    await render(hbs`<PasswordValidator />`);
+
+    console.log('component', component);
+
+    assert.ok(component);
+    assert.strictEqual(component.label, 'Password:');
+  });
+
+  test('it fails blank password', async function (assert) {
+    await render(hbs`<PasswordValidator />`);
+
+    assert.false(component.hasError);
+    await component.submit();
+
+    console.log('component.hasError', component.hasError);
+
+    assert.true(component.hasError);
+  });
+
+  test('it fails short password', async function (assert) {
+    await render(hbs`<PasswordValidator />`);
+
+    await component.fillIn('abc');
+    await component.submit();
+    assert.true(component.hasError);
+  });
+
+  test('it passes valid password', async function (assert) {
+    await render(hbs`<PasswordValidator />`);
+
+    await component.fillIn('abcde');
+    assert.false(component.hasError);
+  });
+});
