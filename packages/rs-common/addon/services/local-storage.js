3c57d66c8fbdf409edf31a9ef8edb60be3de3b2e
Michael Chadwick <michael.chadwick@gmail.com>
2024-11-01 14:36:51 -0700

    removed unneeded constructor

--- packages/rs-common/addon/services/local-storage.js
+++ packages/rs-common/addon/services/local-storage.js
@@ -2,20 +2,6 @@ import Service from '@ember/service';
 import ENV from 'frontend/config/environment';
 
 export default class LocalStorageService extends Service {
-  constructor() {
-    super(...arguments);
-
-    if (!localStorage.getItem(ENV.APP.LOCAL_STORAGE_KEY)) {
-      console.log('initializing LS');
-      localStorage.setItem(
-        ENV.APP.LOCAL_STORAGE_KEY,
-        JSON.stringify({
-          debugExpanded: false,
-        }),
-      );
-    }
-  }
-
   get(key) {
     const settings = JSON.parse(localStorage.getItem(ENV.APP.LOCAL_STORAGE_KEY));
     return settings[key];
