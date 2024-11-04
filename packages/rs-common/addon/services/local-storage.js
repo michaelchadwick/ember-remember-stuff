f6eca120471e249fe870c5328ba19c5d59277e61
Michael Chadwick <michael.chadwick@gmail.com>
2024-11-04 08:09:39 -0800

    added more error checking for localStorage service

--- packages/rs-common/addon/services/local-storage.js
+++ packages/rs-common/addon/services/local-storage.js
@@ -1,15 +1,50 @@
-import Service from '@ember/service';
+import Service, { inject as service } from '@ember/service';
 import ENV from 'frontend/config/environment';
 
 export default class LocalStorageService extends Service {
-  get(key) {
+  @service intl;
+
+  get(item = null) {
     const settings = JSON.parse(localStorage.getItem(ENV.APP.LOCAL_STORAGE_KEY));
-    return settings[key];
+
+    if (settings) {
+      if (item) {
+        const keyVal = settings[item];
+
+        if (keyVal) {
+          return settings[item];
+        } else {
+          return this.intl.t('errors.lsGetItemFail', {
+            keyName: ENV.APP.LOCAL_STORAGE_KEY,
+            itemName: item,
+          });
+        }
+      } else {
+        return settings;
+      }
+    } else {
+      // return this.intl.t('errors.lsGetFail', { keyName: ENV.APP.LOCAL_STORAGE_KEY });
+      return localStorage.setItem(
+        ENV.APP.LOCAL_STORAGE_KEY,
+        JSON.stringify({
+          detailsGalleryExpanded: false,
+        }),
+      );
+    }
   }
 
-  set(key, val) {
+  set(item, val) {
     const settings = JSON.parse(localStorage.getItem(ENV.APP.LOCAL_STORAGE_KEY));
-    settings[key] = val;
-    localStorage.setItem(ENV.APP.LOCAL_STORAGE_KEY, JSON.stringify(settings));
+
+    if (settings) {
+      settings[item] = val;
+      localStorage.setItem(ENV.APP.LOCAL_STORAGE_KEY, JSON.stringify(settings));
+    } else {
+      return this.intl.t('errors.lsSetItemFail', {
+        keyName: ENV.APP.LOCAL_STORAGE_KEY,
+        itemName: item,
+        val: val,
+      });
+    }
   }
 }
