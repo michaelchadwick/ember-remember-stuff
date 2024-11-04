1662772956c57a5df4e311b5fd71b7cc557e2bbd
Michael Chadwick <michael.chadwick@gmail.com>
2024-11-04 13:53:33 -0800

    reduced variables for DetailsGallery component which reduced markup

--- packages/rs-common/addon/components/details-gallery.js
+++ packages/rs-common/addon/components/details-gallery.js
@@ -1,12 +1,14 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import scrollIntoView from 'scroll-into-view';
 import ENV from 'frontend/config/environment';
 
 export default class DetailsGalleryComponent extends Component {
-  @tracked env = ENV.environment;
-  @tracked envApp = ENV.APP;
+  get audioPath() {
+    return ENV.environment == 'production'
+      ? ENV.APP.AUDIO_PLAYER_FILE_REMOTE
+      : ENV.APP.AUDIO_PLAYER_FILE_LOCAL;
+  }
 
   get isExpanded() {
     return this.args.detailsGalleryIsExpanded;
