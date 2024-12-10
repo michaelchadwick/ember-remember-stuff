0f91f5144b58f3ebec3c799f080bc92cc61883d9
Michael Chadwick <michael.chadwick@gmail.com>
2024-12-10 15:29:13 -0800

    allowed for data to be passed to Chart component

---
+++ packages/frontend/tests/integration/components/chart-test.js
@@ -0,0 +1,29 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'frontend/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | chart', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    this.donutData = [
+      {
+        label: 'Super Cool',
+        data: 100,
+        description: 'Lorem Ipsum',
+      },
+      {
+        label: 'Very Cool',
+        data: 200,
+        description: 'Long text description here.',
+      },
+    ];
+
+    await render(hbs`<Chart @name='donut' @data={{this.donutData}} />`);
+
+    assert.dom('.simple-chart').exists();
+    assert.dom('.simple-chart > svg').exists();
+    assert.dom('.simple-chart > svg > g.chart > g.slice > text').hasText('Super Cool');
+  });
+});
--- packages/rs-common/addon/components/chart.js
+++ packages/rs-common/addon/components/chart.js
@@ -7,65 +7,65 @@ export default class ChartComponent extends Component {
   @tracked tooltipContent = null;
   @tracked tooltipTitle = null;
 
-  donutData = [
-    {
-      data: 180,
-      description: '<strong>Super cool</strong> things!',
-      label: '18%',
-      meta: {
-        id: 10,
-      },
-      percentage: '18',
-      title: 'Super Cool Things',
-    },
-    {
-      data: 280,
-      description: 'Uh, moderately fine things, I suppose.',
-      label: '28%',
-      meta: {
-        id: 20,
-      },
-      percentage: '28',
-      title: 'Moderately Fine Things',
-    },
-    {
-      data: 220,
-      description: 'Very awesome things, man.',
-      label: '22%',
-      meta: {
-        id: 30,
-      },
-      percentage: '22',
-      title: 'Very Awesome Things',
-    },
-    {
-      data: 200,
-      description: 'Well, these are sufficient things.',
-      label: '20%',
-      meta: {
-        id: 40,
-      },
-      percentage: '20',
-      title: 'Sufficient Things',
-    },
-    {
-      data: 120,
-      description: 'Look, these things are just kinda neat, I guess.',
-      label: '12%',
-      meta: {
-        id: 50,
-      },
-      percentage: '12',
-      title: 'Kinda Neat Things',
-    },
-  ];
+  get donutData() {
+    return (
+      this.args.data ?? [
+        {
+          data: 180,
+          description: '<strong>Super cool</strong> things!',
+          label: '18%',
+          meta: {
+            id: 10,
+          },
+          percentage: '18',
+          title: 'Super Cool Things',
+        },
+        {
+          data: 280,
+          description: 'Uh, moderately fine things, I suppose.',
+          label: '28%',
+          meta: {
+            id: 20,
+          },
+          percentage: '28',
+          title: 'Moderately Fine Things',
+        },
+        {
+          data: 220,
+          description: 'Very awesome things, man.',
+          label: '22%',
+          meta: {
+            id: 30,
+          },
+          percentage: '22',
+          title: 'Very Awesome Things',
+        },
+        {
+          data: 200,
+          description: 'Well, these are sufficient things.',
+          label: '20%',
+          meta: {
+            id: 40,
+          },
+          percentage: '20',
+          title: 'Sufficient Things',
+        },
+        {
+          data: 120,
+          description: 'Look, these things are just kinda neat, I guess.',
+          label: '12%',
+          meta: {
+            id: 50,
+          },
+          percentage: '12',
+          title: 'Kinda Neat Things',
+        },
+      ]
+    );
+  }
 
   donutHover = restartableTask(async (obj) => {
-    console.log('donutHover', obj);
-    console.log('isIcon?', this.args.isIcon ? 'icon' : 'not icon');
-
-    await timeout(0);
-
+    await timeout(100);
     if (this.args.isIcon || !obj || obj.empty) {
       this.tooltipTitle = null;
       this.tooltipContent = null;
