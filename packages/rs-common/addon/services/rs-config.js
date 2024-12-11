f7a8c08c5b0aa47731eed96309d044e3a6092dd5
Michael Chadwick <michael.chadwick@gmail.com>
2024-12-11 10:52:31 -0800

    temp debug line

--- packages/rs-common/addon/services/rs-config.js
+++ packages/rs-common/addon/services/rs-config.js
@@ -30,6 +30,8 @@ export default class RsConfigService extends Service {
       statusText: 'OK',
     };
 
+    console.log('ENV.environment', ENV.environment);
+
     if (ENV.environment != 'prod') {
       if (!this._configPromise) {
         this._configPromise = this.fetch.getJsonFromApiHost('?config');
