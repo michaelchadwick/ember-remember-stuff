cc655ab2001e3422976130dd3247939a3a3de6a3
Michael Chadwick <michael.chadwick@gmail.com>
2024-12-20 14:23:59 -0800

    updated get-element custom modifier and test to throw error on missing callback

---
+++ packages/frontend/tests/integration/modifiers/get-element-test.js
@@ -0,0 +1,29 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'test-app/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Modifier | get-element', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it passes element to a valid callback', async function (assert) {
+    this.rootElement = null;
+    this.setRootElement = (element) => {
+      this.rootElement = element;
+    };
+    assert.strictEqual(this.rootElement, null);
+    await render(hbs`<div id='root-element' {{get-element this.setRootElement}}></div>`);
+    assert.strictEqual(this.rootElement, document.getElementById('root-element'));
+  });
+
+  test('it fails when no callback is given', async function (assert) {
+    const renderWithoutCallback = async () => {
+      await render(hbs`<div {{get-element}}></div>`);
+    };
+    assert.throws(
+      renderWithoutCallback,
+      /get-element modifier expects a callback as the first positional argument/,
+      'Throws an error when no callback is provided',
+    );
+  });
+});
--- packages/rs-common/addon/modifiers/get-element.js
+++ packages/rs-common/addon/modifiers/get-element.js
@@ -2,8 +2,8 @@ import { modifier } from 'ember-modifier';
 
 export default modifier(function getElement(element, [callback]) {
   if (typeof callback === 'function') {
-    callback(element); // Pass the element to the provided callback
+    callback(element);
   } else {
-    console.warn('get-element modifier expects a callback as the first positional argument.');
+    throw new Error('get-element modifier expects a callback as the first positional argument');
   }
 });
