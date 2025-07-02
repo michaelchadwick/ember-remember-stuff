2de6e38ca6f6fa85f6bb1633ce0ff384908f226e
Michael Chadwick <michael.chadwick@gmail.com>
2025-07-02 11:31:21 -0700

    update for ember-cli-code-coverage bug; refactor

--- packages/frontend/tests/test-helper.js
+++ packages/frontend/tests/test-helper.js
@@ -21,51 +21,62 @@ setRunOptions({
 //   }
 // });
 
+const displayTestStats = (details) => {
+  // console some helpful stats
+  console.info(
+    'Total: ' +
+      details.total +
+      ' Failed: ' +
+      details.failed +
+      ' Passed: ' +
+      details.passed +
+      ' Runtime: ' +
+      details.runtime / 1000 +
+      's',
+  );
+};
+const notify = () => {
+  console.info('notify() TODO');
+  //   if (Notification.permission === 'granted') {
+  //     new Notification('Test Suite Finished');
+  //   }
+  //   let audio = new Audio('../job_done.mp3');
+  //   audio.play().catch((error) => {
+  //     console.error('Failed to play notification sound:', error);
+  //   });
+  //   const { exec } = require('child_process');
+  //   const cmd = `../test-done-alert.php`;
+  //   process.exec(cmd, (error, stderr) => {
+  //     if (error) {
+  //       console.log(`error: ${error.message}`);
+  //     }
+  //     if (stderr) {
+  //       console.log(`stderr: ${stderr}`);
+  //     }
+  //   });
+};
+
 //Needed for: https://github.com/testem/testem/issues/1577
 //See: https://github.com/ember-cli-code-coverage/ember-cli-code-coverage/issues/420
-if (typeof Testem !== 'undefined') {
-  //eslint-disable-next-line no-undef
-  Testem?.afterTests(function (config, data, callback) {
-    forceModulesToBeLoaded();
-    sendCoverage(callback);
-  });
-} else if (typeof QUnit !== 'undefined') {
+if (config.APP.isRunningWithServerArgs) {
+  // until Testem is patched, this will fail to POST coverage in CI mode (running tests with -s or --server as an argument)
+  // Ref: https://github.com/testem/testem/issues/1577
   QUnit.done(async function (details) {
-    console.info(
-      'Total: ' +
-        details.total +
-        ' Failed: ' +
-        details.failed +
-        ' Passed: ' +
-        details.passed +
-        ' Runtime: ' +
-        details.runtime / 1000 +
-        's',
-    );
-
     forceModulesToBeLoaded();
     await sendCoverage();
 
-    // if (Notification.permission === 'granted') {
-    //   new Notification('Test Suite Finished');
-    // }
+    displayTestStats(details);
+    notify();
 
-    // let audio = new Audio('../job_done.mp3');
-    // audio.play().catch((error) => {
-    //   console.error('Failed to play notification sound:', error);
-    // });
+    console.info('config.APP.isRunningWithServerArgs: TRUE');
+  });
+} else {
+  //eslint-disable-next-line no-undef
+  Testem.afterTests(function (config, data, callback) {
+    forceModulesToBeLoaded();
+    sendCoverage(callback);
 
-    // const { exec } = require('child_process');
-    // const cmd = `../test-done-alert.php`;
-
-    // process.exec(cmd, (error, stderr) => {
-    //   if (error) {
-    //     console.log(`error: ${error.message}`);
-    //   }
-    //   if (stderr) {
-    //     console.log(`stderr: ${stderr}`);
-    //   }
-    // });
+    console.info('config.APP.isRunningWithServerArgs: FALSE');
   });
 }
 
@@ -73,5 +84,7 @@ setAdapter(new DefaultAdapter());
 setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
+
+console.info('INFO: starting QUnit...');
 
 start();
