8c4a009c8de65b48480f51d6d98ccc1206590549
Michael Chadwick <michael.chadwick@gmail.com>
2025-07-11 15:23:04 -0700

    update template linting

--- packages/rs-common/.template-lintrc.js
+++ packages/rs-common/.template-lintrc.js
@@ -1,9 +1,12 @@
 'use strict';
 
 module.exports = {
-  plugins: ['ember-template-lint-plugin-prettier'],
-  extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended'],
+  extends: 'recommended',
   rules: {
+    'no-implicit-this': {
+      //our helpers which do not take arguments have to be listed here
+      allow: ['browser-timezone', 'noop'],
+    },
     'no-bare-strings': true,
   },
 };
