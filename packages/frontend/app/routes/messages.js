9ee5802a5db659530c1155a0ce7143f3f66505a4
Michael Chadwick <michael.chadwick@gmail.com>
2025-07-15 14:10:36 -0700

    added some placeholder route files to appease tests

---
+++ packages/frontend/app/routes/messages.js
@@ -0,0 +1,7 @@
+import Route from '@ember/routing/route';
+
+export default class MessagesRoute extends Route {
+  model() {
+    return null;
+  }
+}
---
+++ packages/frontend/app/routes/upload.js
@@ -0,0 +1,7 @@
+import Route from '@ember/routing/route';
+
+export default class UploadRoute extends Route {
+  model() {
+    return null;
+  }
+}
