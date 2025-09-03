895be8a453829f43a771e6c1fa44e3008628f81b
Michael Chadwick <michael.chadwick@gmail.com>
2025-09-03 08:37:03 -0700

    updated HtmlEditor component to be like ilios one

--- packages/rs-common/addon/utils/load-quill-editor.js
+++ packages/rs-common/addon/utils/load-quill-editor.js
@@ -1,20 +1,58 @@
 export async function loadQuillEditor() {
   const { default: QuillEditor } = await import('quill');
+  const undoIcon = await import('quill/assets/icons/undo.svg');
+  const redoIcon = await import('quill/assets/icons/redo.svg');
 
-  // Quill doesn't include redo/undo icons by default, so gotta hack 'em in
+  // Quill doesn't include redo/undo icons by default,
+  // so have to hack in from `quill/assets/icons/[undo|redo].svg`
+  // https://github.com/slab/quill/issues/885#issuecomment-609384079
   const icons = QuillEditor.import('ui/icons');
-  icons['undo'] = `
-    <svg viewbox="0 0 18 18">
-      <polygon class="ql-fill ql-stroke" points="6 10 4 12 2 10 6 10"></polygon>
-      <path class="ql-stroke" d="M8.09,13.91A4.6,4.6,0,0,0,9,14,5,5,0,1,0,4,9"></path>
-    </svg>
-  `;
-  icons['redo'] = `
-    <svg viewbox="0 0 18 18">
-      <polygon class="ql-fill ql-stroke" points="12 10 14 12 16 10 12 10"></polygon>
-      <path class="ql-stroke" d="M9.91,13.91A4.6,4.6,0,0,1,9,14a5,5,0,1,1,5-5"></path>
-    </svg>
-  `;
+  icons['undo'] = undoIcon.default;
+  icons['redo'] = redoIcon.default;
+
+  // Quill automatically adds target="_blank" to inserted links by default.
+  // This overrides the base Link Blot to allow for the target to be customized.
+  const Link = QuillEditor.import('formats/link');
+  class SmartLinkBlot extends Link {
+    static create(value) {
+      const node = super.create(value);
+
+      const { href, blank } = value || {};
+
+      node.setAttribute('href', href);
+
+      if (blank) {
+        node.setAttribute('target', '_blank');
+      } else {
+        node.removeAttribute('target');
+      }
+
+      return node;
+    }
+    static formats(domNode) {
+      return { href: domNode.getAttribute('href'), blank: domNode.getAttribute('target') };
+    }
+    format(name, value) {
+      if (name !== this.statics.blotName || !value) {
+        super.format(name, value);
+      } else {
+        // passed in when creating a new link
+        if (typeof value === 'object') {
+          const { href, blank } = value;
+          this.domNode.setAttribute('href', this.constructor.sanitize(href));
+
+          if (blank) {
+            this.domNode.setAttribute('target', '_blank');
+          }
+        }
+        // passed in when editing an existing link
+        else {
+          this.domNode.setAttribute('href', this.constructor.sanitize(value));
+        }
+      }
+    }
+  }
+  QuillEditor.register(SmartLinkBlot, true);
 
   return {
     QuillEditor,
